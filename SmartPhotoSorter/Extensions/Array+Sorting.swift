64b4cc023d0a1955a806bf8cc013a033390d5d2f
Michael Rommel <rommel@365farmnet.com>
2018-12-10 07:19:41 +0100

    add some extension functions to array

--- SmartPhotoSorter/Extensions/Array+Sorting.swift
+++ SmartPhotoSorter/Extensions/Array+Sorting.swift
@@ -10,18 +10,69 @@ import Foundation
 
 extension Array where Element: Comparable {
 
-	func bubbleSortCount(by areInIncreasingOrder: ((Element, Element) -> Bool) = (<)) -> Int {
+	func bubbleSortCount(by orderingFunc: ((Element, Element) -> Bool) = (<)) -> Int {
 
 		var data = self
 		var numberOfSwaps = 0
 
-		for i in 0..<(data.count - 1) { // 1
-			for j in 0..<(data.count - i - 1) where areInIncreasingOrder(data[j + 1], data[j]) { // 2
+		for i in 0..<(data.count - 1) {
+			for j in 0..<(data.count - i - 1) where orderingFunc(data[j + 1], data[j]) {
 				data.swapAt(j, j + 1)
 				numberOfSwaps = numberOfSwaps + 1
 			}
 		}
 
 		return numberOfSwaps
+	}
+}
+
+extension Array where Element: Equatable {
+
+	public mutating func remove(_ item: Element) {
+		var index = 0
+		while index < self.count {
+			if self[index] == item {
+				self.remove(at: index)
+			} else {
+				index += 1
+			}
+		}
+	}
+
+	public func array(removing item: Element) -> [Element] {
+		var result = self
+		result.remove(item)
+		return result
+	}
+
+	public subscript(index: Int, default defaultValue: @autoclosure () -> Element) -> Element {
+		guard index >= 0, index < endIndex else {
+			return defaultValue()
+		}
+
+		return self[index]
+	}
+}
+
+extension Array {
+
+	mutating func shuffle() {
+		if count < 2 { return }
+		for index in 0..<count - 1 {
+			let newIndex = Int(arc4random_uniform(UInt32(count - index))) + index
+			guard index != newIndex else { continue }
+			self.swapAt(index, newIndex)
+		}
+	}
+
+	func shuffled() -> [Element] {
+		var list = self
+		for index in 0..<list.count {
+			let newIndex = Int(arc4random_uniform(UInt32(list.count - index))) + index
+			if index != newIndex {
+				list.swapAt(index, newIndex)
+			}
+		}
+		return list
 	}
 }
