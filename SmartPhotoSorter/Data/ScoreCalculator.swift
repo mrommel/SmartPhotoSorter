58cf7732ddad555f3fb336c31289b02145ef2908
Michael Rommel <michael.rommel.berlin@web.de>
2018-12-13 14:06:41 +0100

    fix scores

--- SmartPhotoSorter/Data/ScoreCalculator.swift
+++ SmartPhotoSorter/Data/ScoreCalculator.swift
@@ -14,11 +14,11 @@ class ScoreCalculator {
 	///
 	/// https://stackoverflow.com/questions/33899599/bubble-sorting-max-number-of-times
 	/// - Parameter arrayOfRanks: input array
-	/// - Returns: score
+	/// - Returns: score in percent
 	func calculateScore(of arrayOfRanks: [Int]) -> Int {
 		let n = arrayOfRanks.count
 		let maximumNumberOfSwapsPossible = n * (n + 1) / 2
 		//return (maximumNumberOfSwapsPossible - arrayOfRanks.bubbleSortCount()) * 5
-		return Int(Double(arrayOfRanks.bubbleSortCount()) / Double(maximumNumberOfSwapsPossible)) // in percent
+		return Int(100.0 - Double(arrayOfRanks.bubbleSortCount()) / Double(maximumNumberOfSwapsPossible) * 100.0) // in percent
 	}
 }
